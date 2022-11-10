6a757d154be3068c6994291adaf435bcb574c95a
Jamilx <99324373+Jamilx@users.noreply.github.com>
2022-11-10 17:29:09 +0200

    v1.0

--- config.py
+++
@@ -1,3 +0,0 @@
-BOT_TOKEN = "5728646135:AAFLTxnR4HGb32fUlUkzYzaAMqBMktEgq8Y"
-APP_URL = "https://vpfcbot.herokuapp.com/" + BOT_TOKEN
-DB_URL = "postgres://avtpkhrrztloky:97e78189899cd74f95f704a94bb37a25bb56c102876eccb945770d2c778c3041@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/d6f0003rv4vv1n"
